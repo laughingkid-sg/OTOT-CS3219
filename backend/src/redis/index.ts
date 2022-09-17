8b22770d2e85e2b34e3c7c6aba318491d5be6785
Zheng Teck <adamgoh1998@gmail.com>
2022-09-17 19:30:32 +0800

    Fix redis environmental variable error

--- backend/src/redis/index.ts
+++ backend/src/redis/index.ts
@@ -2,7 +2,7 @@ import { RedisClientType, createClient } from "redis";
 require("dotenv").config();
 
 const redisConnection: RedisClientType = createClient({
-    url: process.env.REDIS_SERVER_URL,
+    url: process.env.REDIS_SERVER,
 });
 
 export { redisConnection };
